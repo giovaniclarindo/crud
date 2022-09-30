48ab85a431357e57b09700bccc2013cef1f772b0
Giovani Clarindo <101987259+giovaniclarindo@users.noreply.github.com>
2022-09-29 21:52:36 -0300

    29/09/2022

---
+++ cadastro_script.php
@@ -0,0 +1,19 @@
+<?php
+
+    include "conexao.php";
+
+    if(mysqli_connect_error()) {
+
+        echo "Problemas para conectar no banco. Verificar dados!";
+        die();
+
+        }
+
+        $sql = "INSERT INTO pessoas (nome, email, telefone, endereco)" . "VALUES ('{$_POST["nome"]}', '{$_POST["email"]}', '{$_POST["telefone"]}', '{$_POST["endereco"]}')";
+
+        $conexao->query($sql);
+        $conexao->close();
+
+        echo "Cadastrado com sucesso!";
+
+        header("refresh:3;url=cadastro.html");
---
+++ conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+$nomeServidor = "localhost";
+$nomeUsuario = "root";
+$senha = "";
+$nomeBanco = "empresa";
+
+$conexao = mysqli_connect($nomeServidor, $nomeUsuario, $senha, $nomeBanco);
+
+if($conexao) {
+
+    echo "ok ";
+
+} else {
+
+    echo "falha";
+
+}
